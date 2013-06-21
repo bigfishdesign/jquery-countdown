1dee81f7b394667a6e1b5aaee5b7f7372316e48a
Scott Cariss <scott@bigfish.co.uk>
2013-06-21 17:19:53 +0100

    Wrap plugin in jQuery Closure so we can use the familiar $ in our plugin.

--- jquery.countdown.js
+++ jquery.countdown.js
@@ -1,88 +1,91 @@
-jQuery.fn.countDown = function(endDate, templateString) {
-	var countDown = {
-		elem: this,
+(function ( $ ) {
 
-		now: 0,
-		second: 1000,
-		minute: 60000,
-		hour: 3600000,
-		day: 86400000,
+	$.fn.countDown = function(endDate, templateString) {
+		var countDown = {
+			elem: this,
 
-		endDate: new Date(endDate).getTime(),
+			now: 0,
+			second: 1000,
+			minute: 60000,
+			hour: 3600000,
+			day: 86400000,
 
-		calculateTimeRemaining: function(t){
-			var timeRemaining = countDown.endDate - countDown.now;
+			endDate: new Date(endDate).getTime(),
 
-			if ( t === 'seconds' ) {
-				return Math.floor((timeRemaining % countDown.minute) / countDown.second);
-			}
-			if ( t === 'minutes' ) {
-				return Math.floor((timeRemaining % countDown.hour) / countDown.minute);
-			}
-			if ( t === 'hours' ) {
-				return Math.floor((timeRemaining % countDown.day) / countDown.hour);
-			}
-			if ( t === 'days' ) {
-				return Math.floor(timeRemaining / countDown.day);
-			}
+			calculateTimeRemaining: function(t){
+				var timeRemaining = countDown.endDate - countDown.now;
 
-			return 0;
-		},
-
-		defaultTemplate: '<div class="countdown">' +
-			'<span class="countdown-number">{{ days }}</span> ' +
-			'<span class="countdown-title">days</span> ' +
-			'<span class="countdown-number">{{ hours }}</span> ' +
-			'<span class="countdown-title">hours</span> ' +
-			'<span class="countdown-number">{{ minutes }}</span> ' +
-			'<span class="countdown-title">minutes</span> ' +
-			'<span class="countdown-number">{{ seconds }}</span> ' +
-			'<span class="countdown-title">seconds</span>',
-
-		parseTemplate: function(templateString, templateTags) {
-			jQuery.each(
-				templateTags,
-				function(key, value) {
-					templateString = templateString.replace(new RegExp('{{\\s*' + key + '\\s*}}', 'gi'), value);
+				if ( t === 'seconds' ) {
+					return Math.floor((timeRemaining % countDown.minute) / countDown.second);
 				}
-			);
+				if ( t === 'minutes' ) {
+					return Math.floor((timeRemaining % countDown.hour) / countDown.minute);
+				}
+				if ( t === 'hours' ) {
+					return Math.floor((timeRemaining % countDown.day) / countDown.hour);
+				}
+				if ( t === 'days' ) {
+					return Math.floor(timeRemaining / countDown.day);
+				}
 
-			return templateString;
-		},
+				return 0;
+			},
 
-		update: function() {
-			var templateTags, templateHTML;
+			defaultTemplate: '<div class="countdown">' +
+				'<span class="countdown-number">{{ days }}</span> ' +
+				'<span class="countdown-title">days</span> ' +
+				'<span class="countdown-number">{{ hours }}</span> ' +
+				'<span class="countdown-title">hours</span> ' +
+				'<span class="countdown-number">{{ minutes }}</span> ' +
+				'<span class="countdown-title">minutes</span> ' +
+				'<span class="countdown-number">{{ seconds }}</span> ' +
+				'<span class="countdown-title">seconds</span>',
 
-			countDown.now = new Date().getTime();
+			parseTemplate: function(templateString, templateTags) {
+				$.each(
+					templateTags,
+					function(key, value) {
+						templateString = templateString.replace(new RegExp('{{\\s*' + key + '\\s*}}', 'gi'), value);
+					}
+				);
 
-			// If the countdown has ended, do nothing
-			if ( countDown.endDate - countDown.now < 0 ) {
-				return;
+				return templateString;
+			},
+
+			update: function() {
+				var templateTags, templateHTML;
+
+				countDown.now = new Date().getTime();
+
+				// If the countdown has ended, do nothing
+				if ( countDown.endDate - countDown.now < 0 ) {
+					return;
+				}
+
+				templateTags = {
+					seconds: countDown.calculateTimeRemaining('seconds'),
+					minutes: countDown.calculateTimeRemaining('minutes'),
+					hours:   countDown.calculateTimeRemaining('hours'),
+					days:    countDown.calculateTimeRemaining('days')
+				};
+
+				// If we don't have a templating function, use the default
+				if ( typeof templateString !== 'string' ) {
+					templateString = countDown.defaultTemplate;
+				}
+
+				templateHTML = countDown.parseTemplate(templateString, templateTags);
+
+				// otherwise, inject the countdown into the promotion
+				countDown.elem.html(templateHTML);
+
+				setTimeout(countDown.update, 500);
 			}
+		};
 
-			templateTags = {
-				seconds: countDown.calculateTimeRemaining('seconds'),
-				minutes: countDown.calculateTimeRemaining('minutes'),
-				hours:   countDown.calculateTimeRemaining('hours'),
-				days:    countDown.calculateTimeRemaining('days')
-			};
+		countDown.update();
 
-			// If we don't have a templating function, use the default
-			if ( typeof templateString !== 'string' ) {
-				templateString = countDown.defaultTemplate;
-			}
-
-			templateHTML = countDown.parseTemplate(templateString, templateTags);
-
-			// otherwise, inject the countdown into the promotion
-			countDown.elem.html(templateHTML);
-
-			setTimeout(countDown.update, 500);
-		}
+		return this;
 	};
 
-	countDown.update();
-
-	return this;
-};
-
+}( jQuery ));
